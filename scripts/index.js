51b1ceb6c2cb11fc3449891252a06ae7a1d8bc8d
Kseniia Kostenko <plushy.skirt@gmail.com>
2024-05-16 17:05:45 +0300

    проект

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 // @todo: Темплейт карточки
 
+// Клонируем содержимое тега template
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
@@ -7,3 +9,37 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const CardTemplate = document.querySelector("#card-template").content;
+
+for (let i = 0; i < initialCards.length; i++) {
+  const mediaCard = createCard(
+    initialCards[i].link,
+    initialCards[i].name,
+    removeCard
+  );
+  displayCards(mediaCard);
+}
+
+function createCard(image, title, deleteCard) {
+  const mediaCard = CardTemplate.querySelector(".places__item").cloneNode(true);
+  const deleteButton = mediaCard.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", function () {
+    deleteCard(mediaCard);
+  });
+
+  mediaCard.querySelector(".card__image").src = image;
+  mediaCard.querySelector(".card__title").textContent = title;
+
+  return mediaCard;
+}
+
+function displayCards(mediaCard) {
+  const places = document.querySelector(".places__list");
+  places.append(mediaCard);
+}
+
+function removeCard(card) {
+  card.remove();
+}
